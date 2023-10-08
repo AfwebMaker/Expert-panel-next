733a2c42282a828594b801a8cd03a6467a53bb69
Amir Hossein Saharkhiz <sahar.khiz.1379@gmail.com>
2023-10-08 10:27:25 +0330

    .

--- src/app/_components/inputs/getClassName.js
+++ src/app/_components/inputs/getClassName.js
@@ -1,27 +1,48 @@
-function getClassName({selfState, focus, error, touched, required, value}) {
-    let className = 'bg-white h-[60px] relative fcc flex-col rounded-md overflow-hidden ring-1 ring-cf-200';
+// function getClassName({selfState, focus, error, touched, required, value}) {
+//     let className = 'bg-white h-[60px] relative fcc flex-col rounded-md overflow-hidden ring-1 ring-cf-200';
 
-    if (selfState !== 'None') {
-        className += ' cursor-pointer';
-    }
+//     if (selfState !== 'None') {
+//         className += ' cursor-pointer';
+//     }
 
-    if (focus) {
-        className += error && touched ? ' ring-2 ring-error' : ' ring-primary-500 ring-2';
-    } else if (required) {
-        switch (selfState) {
-            case 'None':
-                className += ' ring-primary-500 ring-2';
-                break;
-            case 'Medium':
-                className += ' border-dashed border-warning border-2';
-                break;
-            case 'High':
-                className += ' ring-error ring-2';
-                break;
-            default:
-                break;
-        }
-    }
+//     if (focus) {
+//         className += error && touched ? ' ring-2 ring-error' : ' ring-primary-500 ring-2';
+//     } else if (required) {
+//         switch (selfState) {
+//             case 'None':
+//                 className += ' ring-primary-500 ring-2';
+//                 break;
+//             case 'Medium':
+//                 className += ' border-dashed border-warning border-2';
+//                 break;
+//             case 'High':
+//                 className += ' ring-error ring-2';
+//                 break;
+//             default:
+//                 break;
+//         }
+//     }
 
-    return className;
+//     return className;
+// }
+
+
+function InputText({ state, title, placeholder, type, className, id, name, required, formik }) {
+    // ... rest of your code ...
+
+    const baseClass = 'bg-white h-[60px] relative fcc flex-col rounded-md overflow-hidden';
+    const cursorClass = selfState !== 'None' ? 'cursor-pointer' : '';
+    const focusClass = focus ? errorCondition ? 'ring-2 ring-error' : 'ring-primary-500 ring-2' : '';
+    const requiredClass = !focus && required ? selfState === 'None' ? 'ring-primary-500 ring-2' : selfState === 'Medium' ? 'border-dashed border-warning border-2' : selfState === 'High' ? 'ring-error ring-2' : 'ring-1 ring-cf-200' : 'ring-1 ring-cf-200';
+
+    return (
+        <div className={`${className}`}>
+            <div onClick={clickHandler} className={`${baseClass} ${cursorClass} ${focusClass} ${requiredClass}`}>
+
+                {/* ... rest of your code ... */}
+
+            </div>
+            {/* ... rest of your code ... */}
+        </div>
+    )
 }

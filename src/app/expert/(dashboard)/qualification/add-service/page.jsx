04426f6c91f7a9dfe42545ea0064f82195ff01ea
whh32 <am.lashkari2017@gmail.com>
2023-10-31 18:21:16 +0330

    update

---
+++ src/app/expert/(dashboard)/qualification/add-service/page.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Page() {
+  return (
+    <div>add service</div>
+  )
+}
+
+export default Page
---
+++ src/app/expert/(dashboard)/qualification/general/first-time/page.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Page() {
+  return (
+    <div>first time</div>
+  )
+}
+
+export default Page

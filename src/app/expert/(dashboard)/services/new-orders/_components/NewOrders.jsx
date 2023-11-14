11829d13c0b45164a34db569b70490b0f98325b6
Amir Hossein Saharkhiz <sahar.khiz.1379@gmail.com>
2023-11-14 17:23:10 +0330

    Complete Tenders components

--- src/app/expert/(dashboard)/services/new-orders/_components/NewOrders.jsx
+++ src/app/expert/(dashboard)/services/new-orders/_components/NewOrders.jsx
@@ -4,7 +4,35 @@ import InfoCard from "@/app/_components/InfoCard"
 import InputFilter from "./InputFilter"
 import ServicesCard from "../../_components/ServicesCard"
 
-function NewOrders() {
+function NewOrders({ data }) {
+    console.log("NewOrder", data)
+
+    function serviceType(number) {
+        let status;
+        switch (number) {
+            case 1:
+                status = "فنی و آیتمی";
+                break;
+            case 2:
+                status = "روز مزد";
+                break;
+            case 3:
+                status = "خدمات فوری";
+                break;
+            case 4:
+                status = "خرده کار";
+                break;
+            case 5:
+                status = "پیمان کار";
+                break;
+            case 6:
+                status = "بازسازی";
+                break;
+            default:
+                status = "نا معلوم"
+        }
+        return status;
+    }
     return (
         <div className='w-full'>
             <InfoCard styleCustom="text-blue-600 w-full">
@@ -12,7 +40,21 @@ function NewOrders() {
             </InfoCard>
             <InputFilter />
             <ul className='w-full grid grid-cols-12 gap-3 mt-10'>
-                <ServicesCard state={"New"} title={"نصب و اجرای شیشه دو جداره"} serviceType={"خدمات فوری"}/>
+                {
+                    data.map((item) => (
+                        <ServicesCard
+                            key={item.id}
+                            state={"New"}
+                            src={item.job_minimal ? item.job_minimal : item.job_mainPicture }
+                            title={item.job_name}
+                            serviceType={serviceType(item.requestType)}
+                            address={item.address}
+                            meterage={item.area}
+                            description={item.description}
+                            time={item.time_Reminding_Seconds_GetPrice * 1000}
+                        />
+                    ))
+                }
             </ul>
         </div>
     )
--- src/app/expert/(dashboard)/services/tenders/page.jsx
+++ src/app/expert/(dashboard)/services/tenders/page.jsx
@@ -1,32 +1,56 @@
 "use client"
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 // components
 import NoContent from "../_components/NoContent"
 import Tenders from "./_components/Tenders"
+import Loading from "@/app/_components/Loading"
 // react icons
 import { HiOutlineClipboardList, HiOutlineClipboardCopy } from 'react-icons/hi'
+// services
+import fetchServices from "@/src/services/core_kg_local/fetchServices";
 
 function Page() {
-  const [data, setData] = useState(true);
+  const [data, setData] = useState([]);
+  const [page, setPage] = useState(1);
+  const [searchInput, setSearchInput] = useState("-");
+  const [loadingPage, setLoadingPage] = useState(true);
+
+  useEffect(() => {
+    fetchServices(page, 10, 2, "-")
+      .then((res) => {
+        console.log("amir", res.data.data)
+        setData(res.data.data);
+      })
+      .catch((error) => {
+        console.log(error);
+      })
+      .finally(() => {
+        setLoadingPage(false)
+      })
+  }, []);
 
   return (
-    <div className="w-[100%] h-[calc(100vh-135px)] md:h-[calc(100vh-190px)] rounded-lg bg-white flex flex-col items-center justify-start py-10 px-5 overflow-y-scroll hideScroll">
-      <div className="w-full h-auto flex flex-col items-center justify-start">
-        {
-          !data ?
-            (<NoContent
-              icon={<HiOutlineClipboardList className="text-5xl" />}
-              title={"شما در هیچ مناقصه ای شرکت نکردید!"}
-              description={"در حال حاضر شما در هیچ مناقصه ای شرکت نکردید و یا قیمت پیشنهادیتان را وارد نکردید برای شرکت از منوی سمت راست سفارشات جدید را انتخاب کنید و با انتخاب یک سفارش در مناقصه شرکت کنید. پش از شرکت در مناقصه یا مناقصات وضعیت مناقصات فعال را در این اینجا می توانید مشاهده کنید."}
-              textBtn={"مشاهده سفارشات جدید"}
-              iconBtn={<HiOutlineClipboardCopy className="text-lg" />}
-              href={"#"}
-            />)
-            :
-            (<Tenders />)
-        }
+    <>
+      {loadingPage && <Loading />}
+      <div className="w-[100%] h-[calc(100vh-135px)] md:h-[calc(100vh-190px)] rounded-lg bg-white flex flex-col items-center justify-start py-10 px-5 overflow-y-scroll hideScroll">
+        <div className="w-full h-auto flex flex-col items-center justify-start">
+          {
+            data && data.lstData && data.lstData.length ?
+              (<Tenders data={data.lstData} />)
+              :
+              (<NoContent
+                icon={<HiOutlineClipboardList className="text-5xl" />}
+                title={"شما در هیچ مناقصه ای شرکت نکردید!"}
+                description={"در حال حاضر شما در هیچ مناقصه ای شرکت نکردید و یا قیمت پیشنهادیتان را وارد نکردید برای شرکت از منوی سمت راست سفارشات جدید را انتخاب کنید و با انتخاب یک سفارش در مناقصه شرکت کنید. پش از شرکت در مناقصه یا مناقصات وضعیت مناقصات فعال را در این اینجا می توانید مشاهده کنید."}
+                textBtn={"مشاهده سفارشات جدید"}
+                iconBtn={<HiOutlineClipboardCopy className="text-lg" />}
+                href={"#"}
+              />)
+          }
+        </div>
       </div>
-    </div>
+    </>
+
   )
 }
 

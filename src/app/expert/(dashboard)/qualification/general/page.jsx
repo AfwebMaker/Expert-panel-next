970a492d3753a5f7eecc1be3d832a2101f5b3526
whh32 <am.lashkari2017@gmail.com>
2023-11-14 14:58:52 +0330

    update qualification

--- src/app/expert/(dashboard)/qualification/general/page.jsx
+++ src/app/expert/(dashboard)/qualification/general/page.jsx
@@ -1,8 +1,30 @@
+'use client'
+
 import React from 'react'
+//components
+import Cart from '../_components/Cart'
+import MainMobileNavigation from '../_components/MainMobileNavigation'
+//react icons
+import { HiOutlineCollection } from 'react-icons/hi'
 
 function Page() {
   return (
-    <div>general</div>
+    <div className="flex w-full h-[calc(100vh-138px)] md:h-auto md:pb-[80px] lg:pb-0 lg:h-auto xl:h-[calc(100vh-152px)] rounded-lg bg-[#F8F9F9] flex-col items-center justify-start relative">
+      <MainMobileNavigation />
+      <div className="overflow-y-scroll hideScroll w-full h-full flex flex-col scroll-smooth px-5 lg:px-0">
+        <div className='py-5 md:px-5 md:bg-white rounded-lg flex flex-col w-full pb-5 md:mb-5'>
+          <div className='w-full text-primary-500 mb-8 hidden md:fcc'>
+            سوالات عمومی احراز صلاحیت
+            <HiOutlineCollection size={24} className='mr-2 font-bold text-lg' />
+          </div>
+          <div className='text-sm font-bold'>سوالات عمومی تایید صلاحیت شده</div>
+          <div className='text-cf-300 font-medium text-xs my-3'>
+            در صورتی که شما در این بخش به تمامی فرم ها پاسخ داده باشید و مورد تایید ما باشد  ادمین های ما آنرا تایید کرده باشند می توانید سرویس جدید اضافه کنید و به سوالات تخصصی صلاحیت هر کدام پاسخ دهید.
+          </div>
+          <Cart />
+        </div>
+      </div>
+    </div>
   )
 }
 
